4a7af9fce1b6005eda9fab339f134c88ab17ebe3
Sergio Pedri <sergio0694@live.com>
2024-06-08 16:55:18 -0700

    Add minimal WinRT hello world project

---
+++ src/hello-minimal-winrt/Class.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace hello_minimal_winrt;
+
+public sealed partial class Class
+{
+    public void SayHello()
+    {
+        Console.WriteLine("Hello, World! From WinRT!");
+    }
+}
