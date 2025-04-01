39b773bb39473c895060585a21c30237f207308c
Michal Strehovský <MichalStrehovsky@users.noreply.github.com>
2025-04-02 00:26:52 +0200

    Use options validator for testing

--- src/TodosApi/AppSettings.cs
+++ src/TodosApi/AppSettings.cs
@@ -1,9 +1,15 @@
-﻿using Microsoft.Extensions.Options;
+﻿﻿using System.ComponentModel.DataAnnotations;
+using Microsoft.Extensions.Options;
 
 namespace TodosApi;
 
 internal class AppSettings
 {
+    [Required(ErrorMessage = """
+                 Connection string not found.
+                 If running locally, set the connection string in user secrets for key 'AppSettings:ConnectionString'.
+                 If running after deployment, set the connection string via the environment variable 'APPSETTINGS__CONNECTIONSTRING'.
+                 """)]
     public required string ConnectionString { get; set; }
 
     public string? JwtSigningKey { get; set; }
@@ -11,23 +17,9 @@ internal class AppSettings
     public bool SuppressDbInitialization { get; set; }
 }
 
-// Changing this to use the Options Validation source generator increases the app size significantly.
-// See https://github.com/dotnet/runtime/issues/106366
-internal class AppSettingsValidator : IValidateOptions<AppSettings>
+[OptionsValidator]
+internal partial class AppSettingsValidator : IValidateOptions<AppSettings>
 {
-    public ValidateOptionsResult Validate(string? name, AppSettings options)
-    {
-        if (string.IsNullOrEmpty(options.ConnectionString))
-        {
-            return ValidateOptionsResult.Fail("""
-                Connection string not found.
-                If running locally, set the connection string in user secrets for key 'AppSettings:ConnectionString'.
-                If running after deployment, set the connection string via the environment variable 'APPSETTINGS__CONNECTIONSTRING'.
-                """);
-        }
-
-        return ValidateOptionsResult.Success;
-    }
 }
 
 internal static class AppSettingsExtensions
