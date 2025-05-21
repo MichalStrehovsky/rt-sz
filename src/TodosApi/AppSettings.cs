99a39dda9220d85eb37c5d0d2db87b3b33c922ef
Michal Strehovský <MichalStrehovsky@users.noreply.github.com>
2025-05-21 21:59:15 +0200

    Revert "Use options validator for testing"
    This reverts commit 39b773bb39473c895060585a21c30237f207308c.

--- src/TodosApi/AppSettings.cs
+++ src/TodosApi/AppSettings.cs
@@ -1,15 +1,9 @@
-﻿﻿using System.ComponentModel.DataAnnotations;
-using Microsoft.Extensions.Options;
+﻿using Microsoft.Extensions.Options;
 
 namespace TodosApi;
 
 internal class AppSettings
 {
-    [Required(ErrorMessage = """
-                 Connection string not found.
-                 If running locally, set the connection string in user secrets for key 'AppSettings:ConnectionString'.
-                 If running after deployment, set the connection string via the environment variable 'APPSETTINGS__CONNECTIONSTRING'.
-                 """)]
     public required string ConnectionString { get; set; }
 
     public string? JwtSigningKey { get; set; }
@@ -17,9 +11,23 @@ internal class AppSettings
     public bool SuppressDbInitialization { get; set; }
 }
 
-[OptionsValidator]
-internal partial class AppSettingsValidator : IValidateOptions<AppSettings>
+// Changing this to use the Options Validation source generator increases the app size significantly.
+// See https://github.com/dotnet/runtime/issues/106366
+internal class AppSettingsValidator : IValidateOptions<AppSettings>
 {
+    public ValidateOptionsResult Validate(string? name, AppSettings options)
+    {
+        if (string.IsNullOrEmpty(options.ConnectionString))
+        {
+            return ValidateOptionsResult.Fail("""
+                Connection string not found.
+                If running locally, set the connection string in user secrets for key 'AppSettings:ConnectionString'.
+                If running after deployment, set the connection string via the environment variable 'APPSETTINGS__CONNECTIONSTRING'.
+                """);
+        }
+
+        return ValidateOptionsResult.Success;
+    }
 }
 
 internal static class AppSettingsExtensions
