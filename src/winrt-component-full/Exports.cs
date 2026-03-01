c726c75f77319e92ddc14b809ab9f6072cd5167e
Sergio Pedri <sergio0694@live.com>
2026-03-01 12:23:54 -0800

    Add WinRTRuntimeClassName attributes and props
    Update Exports.cs to add two new properties and to annotate several types with WinRTRuntimeClassName. Added CustomWWW Url (returns null) and CustomPropertyStructType property (returns new instance). Applied [WinRTRuntimeClassName] attributes mapping to *Impl runtime class names for DisposableClass, CustomReadOnlyDictionary, StaticClass, and NonActivatableFactory to control the WinRT runtime class names used by these types.

--- src/winrt-component-full/Exports.cs
+++ src/winrt-component-full/Exports.cs
@@ -205,6 +205,8 @@ namespace winrt_component_full
     {
         public int Number { get; } = 4;
         public string Value => "CsWinRT";
+        public CustomWWW Url => null;
+        public CustomPropertyStructType CustomPropertyStructType => new CustomPropertyStructType();
     }
 
     [GeneratedBindableCustomProperty]
@@ -623,6 +625,7 @@ namespace winrt_component_full
         }
     }
 
+    [WinRTRuntimeClassName("winrt_component_full.DisposableClassImpl")]
     public sealed class DisposableClass : IDisposable
     {
         public bool IsDisposed { get; set; }
@@ -738,6 +741,7 @@ namespace winrt_component_full
         }
     }
 
+    [WinRTRuntimeClassName("winrt_component_full.CustomReadOnlyDictionaryImpl")]
     public sealed class CustomReadOnlyDictionary : IReadOnlyDictionary<string, BasicStruct>
     {
         private readonly CustomDictionary _dictionary;
@@ -986,6 +990,7 @@ namespace winrt_component_full
         }
     }
 
+    [WinRTRuntimeClassName("winrt_component_full.StaticClassImpl")]
     public static class StaticClass
     {
         public static int GetNumber()
@@ -1980,6 +1985,7 @@ namespace winrt_component_full
         }
     }
 
+    [WinRTRuntimeClassName("winrt_component_full.NonActivatableFactoryImpl")]
     public static class NonActivatableFactory
     {
         public static NonActivatableType Create()
